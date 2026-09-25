a75bc43a610eed81fe17b458b89ffaaab2ef32c2
sildory <159666916+sildory@users.noreply.github.com>
2026-09-25 22:20:18 +0300

    Create __init__.py

---
+++ kit/__init__.py
@@ -0,0 +1,25 @@
+"""
+Пакет процедурных префабов и генераторов сцен 3DVNOSANIM.
+"""
+
+from kit.prefabs import (
+    make_cables,
+    make_ocean,
+    make_rain,
+    make_neon_sign,
+    make_searchlight,
+    make_softbox,
+    make_city_block,
+    import_model,
+)
+
+__all__ = [
+    "make_cables",
+    "make_ocean",
+    "make_rain",
+    "make_neon_sign",
+    "make_searchlight",
+    "make_softbox",
+    "make_city_block",
+    "import_model",
+]
